f8b1c0da6b6692f329a62eee3a86eeceaa8dd611
Niklas19 <n.amberg@hotmail.de>
2019-04-25 11:52:36 +0200

    Added basic visualization files

---
+++ src/powertac_logfiles/vis/app.py
@@ -0,0 +1,45 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+### Additional Styles
+colors = {
+    'background': '#696969',
+    'text': 'white'
+}
+
+
+
+
+app.layout = html.Div(style={'backgroundColor': colors['background'], 'color': colors['text']},children=[
+
+    html.H1(children='PowerTAC Analyzer'),
+
+    html.Div(children='Dash: A web application framework for Python.',style={
+        'textAlign': 'right',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ src/powertac_logfiles/vis/components.py
@@ -0,0 +1,18 @@
+import dash_html_components as html
+import pandas as pd
+
+
+class components:
+    df = pd.read_csv('/Users/Niklas/dev/powertac_analyzer/powertac_logfiles/data/processed/PowerTAC_2018_Finals_1_BrokerAccounting.csv')
+
+
+    def generateTable(dataframe, max_rows=10):
+        return html.Table(
+            # Header
+            [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+            # Body
+            [html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))]
+        )
