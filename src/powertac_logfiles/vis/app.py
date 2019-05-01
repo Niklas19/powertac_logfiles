5402d4e0f73a9906010c5df0d04114a9d9ae1ad2
Niklas19 <n.amberg@hotmail.de>
2019-05-01 13:26:26 +0200

    Session storage added

--- src/powertac_logfiles/vis/app.py
+++ src/powertac_logfiles/vis/app.py
@@ -3,10 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 import powertac_logfiles.vis.components as cs
 
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app = dash.Dash(__name__)
 app.title = 'PowerTAC Analyzer'
+app.config['suppress_callback_exceptions']=True
 
 ### Additional Styles
 colors = {
@@ -14,37 +17,87 @@ colors = {
     'text': 'white'
 }
 
+graph1 = cs.plot_broker_acc()
+table = cs.generate_table()
 
 
-
-app.layout = html.Div(style={'backgroundColor': colors['background'], 'color': colors['text']},children=[
-
-    html.H1(children='PowerTAC Analyzer'),
-
-    html.Div(children='Dash: A web application framework for Python.',style={
-        'textAlign': 'right',
-        'color': colors['text']
-    }),
-
-    cs.generate_table(),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+app.layout = html.Div([
+    html.Header([
+        html.H1(children='PowerTAC Analysis Tool'),
+        html.Img(src='/assets/powertac_header.png'),
+        dcc.Store(id='session', storage_type='session')
+    ]),
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='About', value='tab-1'),
+        dcc.Tab(label='Market details', value='tab-2'),
+        dcc.Tab(label='KPIs', value='tab-3'),
+        dcc.Tab(label='Settings', value='tab-4'),
+    ]),
+    html.Div(id='tabs-content')
 ])
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+
+def render_content(tab):
+# Tab
+    if tab == 'tab-1':
+        return html.Div([
+            html.H1(children='About the PowerTAC Analyses Tool'),
+            table])
+
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H1(children='Market details'),
+            graph1
+        ])
+
+    elif tab == 'tab-3':
+        return html.Div([
+            html.H1(children='KPIS'),
+            html.Div(dcc.Input(id='input-box', type='text')),
+            html.Button('Save to Storage', id='btn_store'),
+            html.Button('Show storage', id='btn_out'),
+            html.Div(id='output-store', children='Hallo')
+        ])
+
+    elif tab == 'tab-4':
+        return html.Div([
+            html.H1(children='Settings'),
+            dcc.Input(id='input-1-keypress',placeholder='Enter a value...',type='text',value=''),
+            html.Div(id='output-keypress')
+        ])
+
+#________Tab4_____________
+@app.callback(Output('output-keypress', 'children'),
+              [Input('input-1-keypress', 'value')])
+def change_output(input1):
+    return u'Input 1 is "{}"'.format(input1)
+
+
+
+#________Tab3______________
+#Input
+@app.callback(Output('session', 'data'),
+              [Input('btn_store', 'n_clicks')],
+               [State('input-box', 'value')])
+def on_click(n_clicks, value):
+    # Give a default data dict with 0 clicks if there's no data.
+    if n_clicks!= 0:
+        data = {'Test': str(value)}
+        return data
+
+
+#Output
+@app.callback(Output('output-store', 'children'),
+                [Input('btn_out', 'n_clicks')],
+                [State('session', 'data')])
+def update_output(n_clicks, data):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        return data.get('Test')
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/powertac_logfiles/vis/components.py
+++ src/powertac_logfiles/vis/components.py
@@ -1,20 +1,75 @@
-import dash_html_components as html
+import dash_table
 import pandas as pd
+import dash_core_components as dcc
+import plotly.graph_objs as go
 
 
-dataframe = pd.read_csv('/Users/Niklas/dev/powertac_analyzer/powertac_logfiles/data/processed/PowerTAC_2018_Finals_1_BrokerAccounting.csv')
+#CONSTANTS
+GAME_SIZE = 7
+
+cols = ['gameId', 'gameSize', 'gameLength', 'AgentUDE', 'Bunnie','COLDPower18', 'CrocodileAgent', 'EWIIS3', 'SPOT', 'VidyutVanika']
+
+df_broker_accounting = pd.read_csv('/Users/Niklas/dev/powertac_analyzer/powertac_logfiles/data/processed/finals_2018_games.csv', nrows=50 ,delimiter=';', usecols=cols)
 
 
-def generate_table(max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
 
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
+def generate_table():
+    dt_tmp = dash_table.DataTable(
+        css=[{
+            'selector': '.dash-cell div.dash-cell-value',
+            'rule': 'display: inline; white-space: inherit; overflow: inherit; text-overflow: inherit;'
+        }],
+        id='table',
+        columns=[{"name": i, "id": i} for i in df_broker_accounting.columns],
+        data=df_broker_accounting.to_dict('records'),
+        style_table={
+            'maxWidth':'95%',
+            'overflowX': 'scroll',
+            'maxHeight': '400px',
+            'overflowY': 'scroll',
+        },
     )
+    return dt_tmp
+
+
+def plot_broker_acc():
+    graph = dcc.Graph(
+        figure=go.Figure(
+            data=[
+                go.Scatter(
+                y=df_broker_accounting['EWIIS3'].values,
+                x=df_broker_accounting['gameId'].values,
+                mode='lines',
+                name='EWIIS3'),
+                go.Scatter(
+                    y=df_broker_accounting['COLDPower18'].values,
+                    x=df_broker_accounting['gameId'].values,
+                    mode='lines',
+                    name='COLDPower18'),
+                go.Scatter(
+                    y=df_broker_accounting['AgentUDE'].values,
+                    x=df_broker_accounting['gameId'].values,
+                    mode='lines',
+                    name='AgentUDE')
+                ],
+            layout=go.Layout(
+                title='Overview Finals',
+                showlegend=True,
+                legend=go.layout.Legend(
+                    x=0,
+                    y=1.0
+                ),
+                margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='my-graph'
+    )
+    return graph
 
 
 
+
+
+if __name__ == '__main__':
+    print(df_broker_accounting.columns)
